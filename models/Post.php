abd2ea66cb4a78c304d8ee3f8a80c985e014c81f
HanumantU <hanumant.uttekar@gmail.com>
2018-09-09 11:51:21 -0700

    Implemented POST method which create the data and store into db.

--- models/Post.php
+++ models/Post.php
@@ -45,4 +45,76 @@ class Post {
         $stmt->execute();
         return $stmt;
     }
+
+    //Getting single post
+    public function read_single(){
+        $query = 'SELECT
+            c.name as cateogry_name,
+            p.id,
+            p.category_id,
+            p.title,
+            p.body,
+            p.author,
+            p.created_at
+        FROM 
+            ' . $this->table.' p
+        LEFT JOIN
+            categories c on p.category_id = c.id
+        WHERE 
+            p.id = ?
+        LIMIT 0,1';
+
+        $stmt = $this->conn->prepare($query);
+
+        //binding id
+        $stmt->bindParam(1, $this->id);
+
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //setting properites
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
+
+
+        return $stmt;
+    }
+
+    //create post
+    public function create() {
+        //create post query
+
+        $query = 'INSERT INTO ' . $this->table . '
+            SET
+                title = :title,
+                body = :body,
+                author = :author,
+                category_id = :category_id';
+                
+        //preparing statement
+        $stmt = $this->conn->prepare($query);
+
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        //binding data
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+        
+        if($stmt->execute()) {
+            return true;
+        }
+
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
 }
